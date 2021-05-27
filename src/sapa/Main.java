9b37f6e5059ffd13d987ec1619d7cbddc7a0d759
Cristina Gómez <cgomez@sapalomera.cat>
2021-05-27 08:55:39 +0200

    sayBye

--- src/sapa/Main.java
+++ src/sapa/Main.java
@@ -2,8 +2,12 @@ package sapa;
 
 public class Main {
 
+    private static void sayBye() {
+        System.out.println("Bye bye");
+    }
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello students!");
+        sayBye();
     }
 }
