2a052cdff9181bc6d74d00f7db8ce785d9c5e80f
Cristina Gómez <cgomez@sapalomera.cat>
2021-05-27 09:01:49 +0200

    hello developers

--- src/sapa/Main.java
+++ src/sapa/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello students!");
+        System.out.println("Hello developers!");
         sayBye();
     }
 }
